94c23c5afeb7c3f02e3cf754b6860b4cd6a34f51
Simon Li <spli@dundee.ac.uk>
2014-08-04 13:57:54 +0100

    Allow run.sh to take a parameter for convenience when testing

--- hadoop-1.2.1/run.sh
+++ hadoop-1.2.1/run.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
 
-env
+INIT=0
+if [ $# -lt 1 ]; then
+	env
+	INIT=1
+	CMD=start
+else
+	CMD="$1"
+fi
 
 IP=$(ip addr show eth0 | sed -nre 's/.*inet ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+).*/\1/p')
 
-sed -i.bak "s/<\/configuration>/\
-	<property><name>dfs.namenode.rpc-address<\/name>\
-	<value>$IP:8020<\/value><\/property>\n\
-<\/configuration>/" /etc/hadoop/hdfs-site.xml
+if [ $INIT -eq 1 ]; then
+	rm -r /var/log/hadoop/*
+	#sed -i.bak "s/<\/configuration>/\
+	#	<property><name>dfs.namenode.rpc-address<\/name>\
+	#	<value>$IP:8020<\/value><\/property>\n\
+#<\/configuration>/" /etc/hadoop/hdfs-site.xml
 
-sed -i.bak "s/hdfs:\/\/localhost:8020/hdfs:\/\/$IP:8020/" \
-	/etc/hadoop/core-site.xml
+	sed -i.bak "s/hdfs:\/\/localhost:8020/hdfs:\/\/$IP:8020/" \
+		/etc/hadoop/core-site.xml
+fi
 
-/etc/init.d/hadoop-datanode start
-/etc/init.d/hadoop-namenode start
-/etc/init.d/hadoop-tasktracker start
-/etc/init.d/hadoop-jobtracker start
+/etc/init.d/hadoop-datanode $CMD
+/etc/init.d/hadoop-namenode $CMD
+/etc/init.d/hadoop-tasktracker $CMD
+/etc/init.d/hadoop-jobtracker $CMD
 
-bash
+if [ $INIT -eq 1 ]; then
+	echo $IP
+	bash
+fi
