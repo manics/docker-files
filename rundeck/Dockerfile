fb230b6a08d3288eb3fa1fe2f5467df9a6dff296
Simon Li <spli@dundee.ac.uk>
2015-04-03 15:41:14 +0100

    Rewrite Dockerfile, use a single volume for data/projects/logs

--- rundeck/Dockerfile
+++ rundeck/Dockerfile
@@ -1,4 +1,4 @@
-FROM centos:centos6
+FROM centos:centos7
 MAINTAINER Simon Li "spli@dundee.ac.uk"
 
 ENV RUNDECK_RELEASE 2.4.2-1.3.GA
@@ -9,12 +9,34 @@ RUN yum -y install java-1.7.0-openjdk && \
 	http://download.rundeck.org/rpm/rundeck-config-${RUNDECK_RELEASE}.noarch.rpm && \
 	yum clean all
 
-ADD run.sh /
+RUN yum -y install sudo && \
+	yum clean all && \
+	echo 'rundeck ALL= (ALL) NOPASSWD: ALL' > /etc/sudoers.d/rundeck && \
+	chmod 440 /etc/sudoers.d/rundeck
+
+RUN mkdir -p /rundeck/data /rundeck/projects /rundeck/logs /rundeck/ssh && \
+	chown rundeck:rundeck /rundeck/*
 
 USER rundeck
 
-VOLUME /config
-VOLUME ["/var/rundeck", "/var/lib/rundeck", "/var/log/rundeck"]
+RUN sed -i \
+	-e 's|/var/lib/rundeck/data|/rundeck/data|' \
+	-e 's|/var/rundeck/projects|/rundeck/projects|' \
+	-e 's|/var/lib/rundeck/logs|/rundeck/logs|' \
+	-e 's|/var/lib/rundeck/.ssh/id_rsa|/rundeck/ssh/id_rsa|' \
+	/etc/rundeck/profile \
+	/etc/rundeck/framework.properties && \
+	sed -i -re 's|#(.*-Drundeck.ssl.config)|\1|' /etc/rundeck/profile
+
+RUN keytool -keystore /etc/rundeck/ssl/keystore -alias rundeck -genkey \
+	-keyalg RSA -keypass adminadmin -storepass adminadmin \
+	-dname "cn=localhost, o=OME, c=UK" && \
+	cp /etc/rundeck/ssl/keystore /etc/rundeck/ssl/truststore && \
+	mv /var/lib/rundeck/.ssh/id_rsa* /rundeck/ssh/
+
+ADD run.sh /
+
+VOLUME ["/etc/rundeck", "/rundeck"]
 
 EXPOSE 4440 4443
 
--- rundeck/run.sh
+++ rundeck/run.sh
@@ -16,40 +16,14 @@ if [ -d /config/ssl/ ]; then
 	fi
 fi
 
-if [ -z "$SERVER_HOSTNAME" ]; then
-	SERVER_HOSTNAME=$(hostname)
-fi
-
-echo "Server hostname: $SERVER_HOSTNAME"
-
-if [ ! -e /etc/rundeck/ssl/keystore ]; then
-	echo "Creating self-signed keystore"
-	keytool -keystore /etc/rundeck/ssl/keystore -alias rundeck -genkey -keyalg RSA -keypass adminadmin -storepass adminadmin  <<EOF
-$SERVER_HOSTNAME
-omedev
-OME
-Dundee
-Scotland
-UK
-yes
-EOF
-
-	if [ ! -e /etc/rundeck/ssl/truststore ]; then
-		cp /etc/rundeck/ssl/keystore /etc/rundeck/ssl/truststore
-	fi
-fi
-
 source /etc/rundeck/profile
 
-sed -i -r \
-	-e "s|(framework.server.name =).*|\1 $SERVER_HOSTNAME|" \
-	-e "s|(framework.server.hostname =).*|\1 $SERVER_HOSTNAME|" \
-	-e "s|(framework.server.port =).*|\1 $RDECK_HTTPS_PORT|" \
-	-e "s|(framework.server.url =).*|\1 https://$SERVER_HOSTNAME:$RDECK_HTTPS_PORT|" \
-	/etc/rundeck/framework.properties
-sed -i -r \
-	-e "s|(grails.serverURL=).*|\1https://$SERVER_HOSTNAME:$RDECK_HTTPS_PORT|" \
-	/etc/rundeck/rundeck-config.properties
-rundeckd="${JAVA_HOME:-/usr}/bin/java ${RDECK_JVM} -cp ${BOOTSTRAP_CP} com.dtolabs.rundeck.RunServer /var/lib/rundeck ${RDECK_HTTP_PORT}"
+if [ -n "$SERVER_HOSTNAME" ]; then
+	RUNDECK_URL=https://$SERVER_HOSTNAME:$RDECK_HTTPS_PORT
+	echo "Setting public URL to: $RUNDECK_URL"
+	sed -i -r -e "s|(grails.serverURL=).*|\1$RUNDECK_URL|" \
+		/etc/rundeck/rundeck-config.properties
+fi
 
-exec $rundeckd
+exec ${JAVA_HOME:-/usr}/bin/java ${RDECK_JVM} -cp ${BOOTSTRAP_CP} com.dtolabs.rundeck.RunServer /var/lib/rundeck ${RDECK_HTTP_PORT}
+
