8a7e152b0b0495f6b911a59e452681941978c088
Simon Li <spli@dundee.ac.uk>
2015-05-14 16:30:35 +0100

    Use query params for domain and re in rundeck-docker-running.py

--- rundeck-cgi-helper/rundeck-docker-running.py
+++ rundeck-cgi-helper/rundeck-docker-running.py
@@ -1,22 +1,30 @@
 #!/usr/bin/env python
 
+import cgi
 import dns.resolver
-import os
+import re
 
-try:
-    q = '*.%s' % os.environ['DOCKER_DOMAIN']
-except KeyError:
-    raise Exception('DOCKER_DOMAIN not defined')
+ps = cgi.FieldStorage()
+d = ps.getvalue('dockerdomain')
+if not d:
+    raise Exception('dockerdomain not defined')
+q = '*.%s' % d
+
+pat = ps.getvalue('namere')
+if not pat:
+    pat = '.*'
+if pat[-1] != '$':
+    pat += '$'
 
 ans = dns.resolver.Resolver().query(q, rdtype='SRV').rrset
 
 nvs = []
 for a in ans:
     parts = a.target.labels
-    assert parts[2:] == ('docker', 'openmicroscopy', 'org', '')
     info, extname = parts[:2]
     intname = info.split(':')[1]
-    nvs.append('{ "name": "%s", "value": "%s" }' % (extname, intname))
+    if re.match(pat, extname):
+        nvs.append('{ "name": "%s", "value": "%s" }' % (extname, intname))
 
 print ('Content-type: application/json\n')
 print ('[%s]' % ','.join(nvs))
